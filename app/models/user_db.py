0714ef7104c66a84fe41dd7b736e557bc4812878
atsushiflee <atsushiflee@icloud.com>
2020-05-04 22:12:28 -0700

    Add select statement

--- app/models/user_db.py
+++ app/models/user_db.py
@@ -9,9 +9,24 @@ class User(db.Model):
     __tablename__ = "user"
 
     user_id = db.Column("user_id", db.Integer, primary_key=True)
-    user_name = db.Column("user_name", db.String(10))
-    user_mail = db.Column("user_mail", db.String(320))
-    user_password = db.Column("user_password", db.String(32))
+    user_name = db.Column("user_name", db.String(10),
+                          unique=True, nullable=False)
+    user_mail = db.Column("user_mail", db.String(320),
+                          unique=True, nullable=False)
+    user_password = db.Column("user_password", db.String(32), nullable=False)
 
     def __repr__(self):
         return f"User('{self.user_name}', '{self.user_mail}', '{self.user_password}')"
+
+    @classmethod
+    def find_user_email_and_name(cls, name, email):
+        exist_user = User.query.filter_by(
+            user_name=name).first()
+        if not exist_user is None:
+            return "User name"
+        exist_email = User.query.filter_by(
+            user_mail=email).first()
+        if not exist_email is None:
+            return "mail address"
+
+        return True
