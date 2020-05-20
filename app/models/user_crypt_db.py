3f915f02fea45c625328ffaa327ca3a7df00bb08
atsushiflee <atsushiflee@icloud.com>
2020-05-19 23:23:34 -0700

    Fix db execute part

--- app/models/user_crypt_db.py
+++ app/models/user_crypt_db.py
@@ -1,12 +1,16 @@
 from app import db
 
 
-class UserCrypt():
-    user_crypt = db.Table('user_crypt',
-                          db.Column("user_id", db.Integer, db.ForeignKey(
-                              "user.user_id"), primary_key=True),
-                          db.Column("crypt_id", db.Integer, db.ForeignKey(
-                              "crypt.crypt_id"), primary_key=True))
+class UserCrypt(db.Model):
 
-    def __repr__(self):
-        return f"UserCrypt('{self.user_id}', '{self.crypt_id}')"
+    __tablename__ = "user_crypt"
+
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        "user.user_id"), primary_key=True)
+
+    crypt_id = db.Column(db.Integer, db.ForeignKey(
+        "crypt.crypt_id"), primary_key=True)
+    num_of_currency = db.Column(db.Float)
+
+    user = db.relationship("User")
+    crypt = db.relationship("Crypt")
