9243c8aa0010af488dea070a99a0aabdebe85641
atsushiflee <atsushiflee@icloud.com>
2020-06-17 18:38:08 -0700

    Add delte and login actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,40 @@
 import crypto from "../api/api";
 import history from "../history";
-import { SIGN_UP, LOGIN, LOGOUT } from "./types";
+import { SIGN_UP, LOGIN, LOGOUT, FETCH_USER, DELETE_USER } from "./types";
 
 export const signUp = (formValues) => async (dispatch) => {
   const response = await crypto.post("/signup", formValues);
   console.log(response);
-  if (!response) {
+  if (response.data) {
+    dispatch({ type: SIGN_UP, payload: response.data });
+    history.push("/form/login");
+  } else {
+    dispatch({ type: SIGN_UP, payload: response.data });
     history.push("/form/signup");
   }
-  dispatch({ type: SIGN_UP, payload: response.data });
-  // history.push("/form/login");
+};
+
+export const login = (formValues) => async (dispatch) => {
+  console.log(formValues);
+  const response = await crypto.post("/login", formValues);
+
+  if (response.data) {
+    console.log("login!");
+    dispatch({ type: LOGIN, payload: response.data });
+    history.push("form/login");
+  } else {
+    console.log("failr");
+    dispatch({ type: LOGIN, payload: response.data });
+    history.push("/form/login");
+  }
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await crypto.get(`/fetch/${id}`);
+  dispatch({ type: FETCH_USER, payload: response.data });
+};
+
+export const deleteUser = (id) => async (dispatch) => {
+  await crypto.delete(`/delete/${id}`);
+  dispatch({ type: DELETE_USER, payload: id });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,5 @@
 export const SIGN_UP = "SIGN_UP";
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
+export const FETCH_USER = "FETCH_USER";
+export const DELETE_USER = "DELETE_USER";
