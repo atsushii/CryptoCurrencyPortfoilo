65385ab73c1e0cfbb026a9c6f70d3a3ea50c2040
atsushiflee <atsushiflee@icloud.com>
2020-06-22 22:42:43 -0700

    Add delete and fetch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,40 +1,45 @@
 import crypto from "../api/api";
 import history from "../history";
-import { SIGN_UP, LOGIN, LOGOUT, FETCH_USER, DELETE_USER } from "./types";
+import {
+  SIGN_UP,
+  LOGIN,
+  LOGOUT,
+  FETCH_USER,
+  DELETE_USER,
+  REFETCH_USER,
+} from "./types";
 
 export const signUp = (formValues) => async (dispatch) => {
   const response = await crypto.post("/signup", formValues);
-  console.log(response);
-  if (response.data) {
-    dispatch({ type: SIGN_UP, payload: response.data });
-    history.push("/form/login");
-  } else {
-    dispatch({ type: SIGN_UP, payload: response.data });
-    history.push("/form/signup");
-  }
+
+  dispatch({ type: SIGN_UP, payload: response.data });
+  history.push("/form/login");
 };
 
 export const login = (formValues) => async (dispatch) => {
-  console.log(formValues);
-  const response = await crypto.post("/login", formValues);
+  const response = await crypto.post("/login", formValues, {
+    credentials: "same-origin",
+  });
 
-  if (response.data) {
-    console.log("login!");
-    dispatch({ type: LOGIN, payload: response.data });
-    history.push("form/login");
-  } else {
-    console.log("failr");
-    dispatch({ type: LOGIN, payload: response.data });
-    history.push("/form/login");
-  }
+  dispatch({ type: LOGIN, payload: response.data });
+  history.push(`/form/delete/${response.data.id}`);
 };
 
 export const fetchUser = (id) => async (dispatch) => {
-  const response = await crypto.get(`/fetch/${id}`);
+  const response = await crypto.get(`/fetch/${id}`, {
+    withCredentials: true,
+  });
   dispatch({ type: FETCH_USER, payload: response.data });
 };
 
 export const deleteUser = (id) => async (dispatch) => {
   await crypto.delete(`/delete/${id}`);
   dispatch({ type: DELETE_USER, payload: id });
+};
+
+export const reFetchUser = () => async (dispatch) => {
+  const response = await crypto.get("/refetch");
+  console.log("refetch");
+  console.log(response.data);
+  dispatch({ type: REFETCH_USER, payload: response.data });
 };
