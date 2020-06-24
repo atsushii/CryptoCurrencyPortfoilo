1411930594cee775a4e0e8526693759f8c0cfb47
atsushiflee <atsushiflee@icloud.com>
2020-06-23 19:38:40 -0700

    Add User Update API

---
+++ client/src/components/form/EditUser.js
@@ -0,0 +1,35 @@
+import _ from "lodash";
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUser, editUser } from "../../actions";
+import UserForm from "./UserForm";
+
+class EditUser extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editUser(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.user) {
+      return <div>Loading User Information</div>;
+    }
+    return (
+      <div>
+        <UserForm
+          initialValues={_.pick(this.props.user, "username", "email")}
+          onSubmit={this.onSubmit}
+          title="Edit a User"
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.user[ownProps.match.params.id] };
+};
+export default connect(mapStateToProps, { fetchUser, editUser })(EditUser);
