0ad7552b80d2b7e74e839d353ec5afb33ba6f108
atsushiflee <atsushiflee@icloud.com>
2020-06-25 19:07:25 -0700

    Create new reducer for portfolio

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import userReducer from "./userReducer";
-
+import portfolioReducer from "./portfolioReducer";
 export default combineReducers({
   form: formReducer,
   user: userReducer,
+  portfolio: portfolioReducer,
 });
