2a36c1e353e4c9734a1d950716fe6b94deb34ea1
atsushiflee <atsushiflee@icloud.com>
2020-06-25 19:05:10 -0700

    Create portfolio component

--- client/src/components/portfolio/Portfolio.js
+++ client/src/components/portfolio/Portfolio.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import { fetchUser, fetch_currency } from "../../actions";
+import { connect } from "react-redux";
 
 class Portfolio extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.match.params.id);
+    this.props.fetch_currency(this.props.match.params.id);
+  }
+
   render() {
-    return "top page";
+    if (!this.props.user || !this.props.portfolio) {
+      return <div>Loading Portfolio</div>;
+    }
+    return (
+      <div>
+        <Header username={this.props.user.username} />
+        <Content
+          currency_list={this.props.portfolio.currency_list}
+          total_value={this.props.portfolio.total_value}
+        />
+        <Footer />
+      </div>
+    );
   }
 }
 
-export default Portfolio;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user[ownProps.match.params.id],
+    portfolio: state.portfolio[ownProps.match.params.id],
+  };
+};
+
+export default connect(mapStateToProps, { fetchUser, fetch_currency })(
+  Portfolio
+);
