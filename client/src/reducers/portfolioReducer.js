ef48568468d10c7b12948585b031dd9c9b72cf4d
atsushiflee <atsushiflee@icloud.com>
2020-06-27 15:30:14 -0700

    Add delete currency reducer

--- client/src/reducers/portfolioReducer.js
+++ client/src/reducers/portfolioReducer.js
@@ -1,9 +1,19 @@
-import { FETCH_CURRENCY } from "../actions/types";
+import {
+  FETCH_CURRENCY,
+  EDIT_CURRENCY,
+  DELETE_CURRENCY,
+} from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_CURRENCY:
       return { ...state, [action.payload.id]: action.payload };
+
+    case EDIT_CURRENCY:
+      return { ...state, [action.payload.id]: action.payload };
+
+    case DELETE_CURRENCY:
+      return "";
 
     default:
       return state;
