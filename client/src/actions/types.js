ee1313cc09cb430253dee6b07faa63fa57b97a75
atsushiflee <atsushiflee@icloud.com>
2020-06-28 14:52:33 -0700

    Add register typw

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,6 +5,7 @@ export const FETCH_USER = "FETCH_USER";
 export const DELETE_USER = "DELETE_USER";
 export const REFETCH_USER = "REFETCH_USER";
 export const EDIT_USER = "EDIT_USER";
+export const REGISTER_CURRENCY = "REGISTER_CURRENCY";
 export const FETCH_CURRENCY = "FETCH_CURRENCY";
 export const EDIT_CURRENCY = "EDIT_CURRENCY";
 export const DELETE_CURRENCY = "DELETE_CURRENCY";
