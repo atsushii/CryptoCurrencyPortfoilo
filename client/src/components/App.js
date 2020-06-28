512c7ab6ea86db675dde6f1922170ea3bcc1beeb
atsushiflee <atsushiflee@icloud.com>
2020-06-28 14:51:26 -0700

    Add route for register page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import RegisterUser from "./form/RegisterUser";
-import LoginForm from "./form/LoginForm";
+import LoginUser from "./form/LoginUser";
+import DeleteUser from "./form/DeleteUser";
+import EditUser from "./form/EditUser";
+import Portfolio from "./portfolio/Portfolio";
+import EditCurrency from "./portfolio/EditCurrency";
+import RegisterCurrency from "./portfolio/RegisterCurrency";
+import DeleteCurrency from "./portfolio/DeleteCurrency";
 import history from "../history";
 
 const App = () => {
@@ -10,7 +16,25 @@ const App = () => {
       <Router history={history}>
         <Switch>
           <Route path="/form/signup" exact component={RegisterUser} />
-          <Route path="/form/login" exact component={LoginForm} />
+          <Route path="/form/login" exact component={LoginUser} />
+          <Route path="/form/edit/:id" exact component={EditUser} />
+          <Route path="/form/delete/:id" exact component={DeleteUser} />
+          <Route path="/portfolio/:id" exact component={Portfolio} />
+          <Route
+            path="/portfolio/registerCurrency/:id"
+            exact
+            component={RegisterCurrency}
+          />
+          <Route
+            path="/portfolio/editCurrency/:id/:symbol"
+            exact
+            component={EditCurrency}
+          />
+          <Route
+            path="/portfolio/deleteCurrency/:id/:symbol"
+            exact
+            component={DeleteCurrency}
+          />
         </Switch>
       </Router>
     </div>
