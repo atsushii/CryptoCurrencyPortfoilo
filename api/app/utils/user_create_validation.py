413b10d5341a41f28d37360548b86db4d1829c20
atsushiflee <atsushiflee@icloud.com>
2020-07-01 14:28:13 -0700

    Add validation for form data

--- api/app/utils/user_create_validation.py
+++ api/app/utils/user_create_validation.py
@@ -1,4 +1,5 @@
 from app.models.user_db import User
+import re
 
 
 class UserCreateValidator():
@@ -14,3 +15,31 @@ class UserCreateValidator():
             form_data["username"], form_data["email"])
 
         return exist_user
+
+    def user_form_validation(self, form_data):
+        """
+        Validate form data
+        """
+
+        # username
+        if len(form_data["username"]) > 10:
+            return False
+
+        # mail
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        if not re.search(regex, form_data["email"]):
+            return False
+
+        # password
+        if len(form_data["password"]) < 8:
+            return False
+        elif not re.search("[0-9]", form_data["password"]):
+            return False
+        elif not re.search("[A-Z]", form_data["password"]):
+            return False
+        elif not re.search("[@_!#$%^&*()<>?/\|}{~:]", form_data["password"]):
+            return False
+        elif form_data["password"] != form_data["confirmPassword"]:
+            return False
+
+        return True
