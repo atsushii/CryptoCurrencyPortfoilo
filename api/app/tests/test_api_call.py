f9d102d3237099ca78a5faa92be1360620fc724a
atsushiflee <atsushiflee@icloud.com>
2020-07-04 23:17:44 -0700

    Test external api call

---
+++ api/app/tests/test_api_call.py
@@ -0,0 +1,36 @@
+import unittest
+from app.utils.execute_api import API
+from app import db, create_test_app
+
+
+class TestApiCall(unittest.TestCase):
+
+    def setUp(self):
+        print("setUp test env")
+
+        # init test env
+        self.app = create_test_app()
+
+        self.api = API()
+
+    def tearDown(self):
+        pass
+
+    def test_call_api(self):
+
+        res = self.api.call_api(currency=["BTC"])
+
+        self.assertNotEqual(res, [])
+
+    def test_data_process(self):
+        currency_data = self.api.call_api(currency=["BTC"])
+
+        currency_list, totla_value = self.api.data_process(currency_data=currency_data,
+                                                           num_of_hold_currency=[5])
+
+        self.assertIsNotNone(totla_value)
+        self.assertNotEqual(currency_list, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
