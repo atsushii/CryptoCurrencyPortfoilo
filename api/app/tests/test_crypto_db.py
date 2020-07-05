034202c72be5816c1500eeb14c95282e7d97549a
atsushiflee <atsushiflee@icloud.com>
2020-07-04 23:15:33 -0700

    Test crypto database

---
+++ api/app/tests/test_crypto_db.py
@@ -0,0 +1,36 @@
+import unittest
+from app import db, create_test_app
+from app.models.crypt_db import Crypt
+
+
+class TestCryptoDB(unittest.TestCase):
+
+    def setUp(self):
+        print("setUp test env")
+
+        # init test env
+        self.app = create_test_app()
+        db.create_all()
+
+        self.crypto = Crypt()
+
+        # init crypto data
+        crypto = Crypt(crypt_id=1, crypt_name="BTC")
+        db.session.add(crypto)
+        db.session.commit()
+
+    def tearDown(self):
+        print("tearDown")
+        db.session.remove()
+        db.drop_all()
+
+    def test_fetch_curency_data_by_name(self):
+        res = self.crypto.find_currency_name(crypt_name="BTC")
+        self.assertIsNotNone(res)
+
+        res = self.crypto.find_currency_name(crypt_name="ETH")
+        self.assertIsNone(res)
+
+
+if __name__ == "__main__":
+    unittest.main()
