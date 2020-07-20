74eac0e3799e3b872907998782c75c210a46b616
atsushiflee <atsushiflee@icloud.com>
2020-07-20 14:27:16 -0700

    Add read config depends on environent

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,39 +1,58 @@
 from flask import Flask
-from app.config import Config, TestConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS, cross_origin
 from flask_session import Session
 from flask_mail import Mail
 from flask_script import Manager
-
+from app.config import DevelopmentConfig, TestConfig, ProductionConfig
+import os
 
 db = SQLAlchemy()
 mail = Mail()
 
 
 def create_app():
+
     app = Flask(__name__)
-    from app.views.views import user_page
-    app.config.from_object(Config)
-    cors = CORS(app, supports_credentials=True)
-    app.register_blueprint(user_page)
-    from app.models.user_db import db
-    from app.models.crypt_db import db
-    from app.models.user_crypt_db import db
-    from app.utils.execute_user_db import db
-    from app.utils.execute_crypt_db import db
-    db.init_app(app)
-    Migrate(app, db)
-    mail.init_app(app)
+    comfig_name = os.environ.get('FLASK_ENV')
 
-    return app
+    # dev and prod environment
+    if comfig_name == "development":
+        from app.views.views import user_page
+        app.config.from_object(DevelopmentConfig)
+        cors = CORS(app, supports_credentials=True)
+        app.register_blueprint(user_page)
+        from app.models.user_db import db
+        from app.models.crypt_db import db
+        from app.models.user_crypt_db import db
+        from app.utils.execute_user_db import db
+        from app.utils.execute_crypt_db import db
+        db.init_app(app)
+        Migrate(app, db)
+        mail.init_app(app)
 
+        return app
 
-def create_test_app():
-    app = Flask(__name__)
-    mail = Mail()
+    elif comfig_name == "production":
 
+        from app.views.views import user_page
+        app.config.from_object(ProductionConfig)
+        cors = CORS(app, supports_credentials=True)
+        app.register_blueprint(user_page)
+        from app.models.user_db import db
+        from app.models.crypt_db import db
+        from app.models.user_crypt_db import db
+        from app.utils.execute_user_db import db
+        from app.utils.execute_crypt_db import db
+        db.init_app(app)
+        Migrate(app, db)
+        mail.init_app(app)
+
+        return app
+
+    # test environment
+    db = SQLAlchemy()
     app.config.from_object(TestConfig)
     from app.views.views import user_page
     app.register_blueprint(user_page)
