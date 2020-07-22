a3278a17a4237a7217a77467801e8384a44a5504
Atsushi Miyamoto <atsushiflee@icloud.com>
2020-07-22 09:01:01 -0700

    Delete build.sh

--- pipeline/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "***********************************"
-echo "**** Building Docker test image ***"
-echo "***********************************"
-#tesr
-docker-compose -f ../docker-compose.test.yml build
-
